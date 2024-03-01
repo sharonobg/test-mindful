96e5ab12bcf40ac0b386fb285fada533beaedd25
sharonobg <sharon@sharonobrien.com>
2024-03-01 13:09:01 -0500

    vercel cors

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,23 @@
+//import {NextResponse} from "next/server";
+
+// export function middleware(){
+    // const res = NextResponse.next()
+    //add the CORS headers to the response
+    // res.headers.append('Access-Control-Allow-Credentials', "true")
+    // res.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+    // res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    // res.headers.append(
+        // 'Access-Control-Allow-Headers',
+        // 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    // )
+// 
+    // return res
+// }
+
 export{default} from 'next-auth/middleware'
 
-export const config = {matcher:[
+export const config = {
+    matcher:[
    // "/transaction/:path*",
     //"/addCategory",  
     "/api/:path*",
