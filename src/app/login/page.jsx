bdbc90de87d1b2004a4e0559970f04e4f22185a2
sharonobg <sharon@sharonobrien.com>
2024-03-01 18:07:43 -0500

    useEff

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import Link from 'next/link';
 import {ToastContainer ,toast,cssTransition,Bounce} from 'react-toastify';
 import GithubButton from '../../components/GithubButton';
