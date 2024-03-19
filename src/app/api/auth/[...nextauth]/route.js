b18ba5c5296d0f28c1b353115bf8f16a37ca7330
sharonobg <sharon@sharonobrien.com>
2024-03-19 19:11:41 -0400

    removing gitub

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 //typescript: import NextAuth, { AuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from 'next-auth/providers/credentials';
 //import {MongoDBAdapter} from '@auth/mongodb-adapter'
 //import clientPromise from '../../../../libs/clientpromise'
@@ -13,10 +12,6 @@ import {NextResponse} from 'next/server'
 export const authOptions = 
     {
         providers:[
-            GitHubProvider({
-                clientId: process.env.GITHUB_ID ?? "",
-                clientSecret: process.env.GITHUB_SECRET ?? "",
-              }),
             CredentialsProvider({
                 type:'credentials',
                 credentials:{
